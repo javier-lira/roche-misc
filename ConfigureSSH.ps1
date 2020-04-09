e4cc1a039dec9ee5d5aaed042224103d4499c087
Javier Lira <javier.lira@roche.com>
2020-04-09 19:04:42 +0200

    Add firewall rule

--- ConfigureSSH.ps1
+++ ConfigureSSH.ps1
@@ -1,6 +1,7 @@
 Expand-Archive -LiteralPath OpenSSH-Win64.zip -DestinationPath "C:\Program Files"
 Rename-Item "C:\Program Files\OpenSSH-Win64" "C:\Program Files\OpenSSH"
 powershell.exe -ExecutionPolicy Bypass -File "C:\Program Files\OpenSSH\install-sshd.ps1"
+New-NetFirewallRule -Name sshd -DisplayName 'OpenSSH Server (sshd)' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22
 Set-Service sshd -StartupType Automatic
 Set-Itemproperty -path 'HKLM:\SOFTWARE\OpenSSH' -Name 'DefaultShell' -value 'C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe'
 net start sshd
