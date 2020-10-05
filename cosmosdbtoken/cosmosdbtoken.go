0c4cbfb7f750db95cf3ba5a8459bf35ab9abfb20
Javier Lira <javier.lira@roche.com>
2020-10-05 14:53:08 +0200

    Make cosmosdbtoken a library

--- cosmosdbtoken/cosmosdbtoken.go
+++ cosmosdbtoken/cosmosdbtoken.go
@@ -1,19 +1,17 @@
-package main
+package cosmosdbtoken
 
 import (
 	"crypto/hmac"
 	"crypto/sha256"
 	"encoding/base64"
-	"flag"
 	"fmt"
 	"net/http"
 	"net/url"
-	"os"
 	"strings"
 	"time"
 )
 
-func getAuthorizationToken(masterKeyBase64 string,
+func GetAuthorizationToken(masterKeyBase64 string,
 	verb string,
 	resourceType string,
 	resourceLink string) (string, string) {
@@ -31,28 +29,4 @@ func getAuthorizationToken(masterKeyBase64 string,
 	auth := fmt.Sprintf("type=master&ver=1.0&sig=%s", sha)
 	authencoded := url.QueryEscape(auth)
 	return authencoded, date
-}
-
-func main() {
-
-	var masterKeyBase64 *string = flag.String("masterkey", "", "Primary key for accessing to cosmosdb")
-	var verb *string = flag.String("verb", "GET", "Method for the HTTP request, e.g. GET, POST or PUT")
-	var resourceType *string = flag.String("resourceType", "docs", "Type of resource that is being requested")
-	var resourceLink *string = flag.String("resourceLink", "", "Identifier for the entity being queried")
-
-	flag.Parse()
-
-	if flag.NFlag() != 4 {
-		fmt.Println("Not all parameters have been set. Please use -h for more information")
-		os.Exit(1)
-	}
-
-	authToken, date := getAuthorizationToken(*masterKeyBase64,
-		*verb,
-		*resourceType,
-		*resourceLink)
-
-	fmt.Println("Authorization: ", authToken)
-	fmt.Println("Date: ", date)
-
 }
